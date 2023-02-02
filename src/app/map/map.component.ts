e4d1cca08411dc3114044ea9e195bae3c9cca626
eddy08 <bhardwajharsh08@gmail.com>
2023-02-02 21:34:17 +0530

    Added Route between two points on Globe 🗺

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -24,270 +24,291 @@ Highcharts.setOptions({
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
-  ngAfterContentChecked() {
-    // this.reflow();
-    this.chartCallback = (chart: any) => {
-      this.Globe = chart;
-      // const pos = chart.fromLatLonToPoint({ lat: 19.07, lon: 72.87 });
+  // ngAfterContentChecked() {
+  //   // this.reflow();
+  //   this.chartCallback = (chart: any) => {
+  //     this.Globe = chart;
+  //     // const pos = chart.fromLatLonToPoint({ lat: 19.07, lon: 72.87 });
 
-      // (chart as ExtendedChart).mapView.zoomBy(3, [pos.x, pos.y]);
-      let that = this;
-      // Function to return an SVG path between two points, with an arc
-      let pointsToPath = (
-        fromPoint: number,
-        toPoint: number,
-        invertArc: boolean = false,
-        curve: number = 0
-      ): any[][] => {
-        const from = chart.mapView.lonLatToProjectedUnits(fromPoint),
-          to = chart.mapView.lonLatToProjectedUnits(toPoint),
-          arcPointX = (from.x + to.x) / (invertArc ? 2 + curve : 2 - curve),
-          arcPointY = (from.y + to.y) / (invertArc ? 2 + curve : 2 - curve);
+  //     // (chart as ExtendedChart).mapView.zoomBy(3, [pos.x, pos.y]);
+  //     let that = this;
+  //     // Function to return an SVG path between two points, with an arc
+  //     let pointsToPath = (
+  //       fromPoint: number,
+  //       toPoint: number,
+  //       invertArc: boolean = false,
+  //       curve: number = 0
+  //     ): any[][] => {
+  //       const from = chart.mapView.lonLatToProjectedUnits(fromPoint),
+  //         to = chart.mapView.lonLatToProjectedUnits(toPoint),
+  //         arcPointX = (from.x + to.x) / (invertArc ? 2 + curve : 2 - curve),
+  //         arcPointY = (from.y + to.y) / (invertArc ? 2 + curve : 2 - curve);
 
-        return [
-          ['M', from.x, from.y],
-          ['Q', arcPointX, arcPointY, to.x, to.y],
-        ];
-      };
+  //       return [
+  //         ['M', from.x, from.y],
+  //         ['Q', arcPointX, arcPointY, to.x, to.y],
+  //       ];
+  //     };
 
-      this.map = {
-        chart: {
-          map: this.topology,
-        },
+  //     this.map = {
+  //       chart: {
+  //         map: this.topology,
+  //       },
 
-        title: {
-          text: 'World Wide Login',
-          floating: true,
-          align: 'left',
-          style: {
-            textOutline: '2px white',
-          },
-        },
+  //       title: {
+  //         text: 'World Wide Login',
+  //         floating: true,
+  //         align: 'left',
+  //         style: {
+  //           textOutline: '2px white',
+  //         },
+  //       },
 
-        subtitle: {
-          text:
-            'Source: <a href="https://mfe-login-app.vercel.app/">Login Application</a><br>' +
-            'Click and drag to rotate globe<br>',
-          floating: true,
-          y: 34,
-          align: 'left',
-        },
+  //       subtitle: {
+  //         text:
+  //           'Source: <a href="https://mfe-login-app.vercel.app/">Login Application</a><br>' +
+  //           'Click and drag to rotate globe<br>',
+  //         floating: true,
+  //         y: 34,
+  //         align: 'left',
+  //       },
 
-        legend: {
-          enabled: false,
-        },
+  //       legend: {
+  //         enabled: false,
+  //       },
 
-        mapNavigation: {
-          enabled: true,
-          enableDoubleClickZoomTo: true,
-          buttonOptions: {
-            verticalAlign: 'bottom',
-          },
-        },
+  //       mapNavigation: {
+  //         enabled: true,
+  //         enableDoubleClickZoomTo: true,
+  //         buttonOptions: {
+  //           verticalAlign: 'bottom',
+  //         },
+  //       },
 
-        mapView: {
-          projection: {
-            name: 'Orthographic',
-            rotation: [60, -30],
-          },
-        },
+  //       mapView: {
+  //         projection: {
+  //           name: 'Orthographic',
+  //           rotation: [60, -30],
+  //         },
+  //       },
 
-        colorAxis: {
-          tickPixelInterval: 100,
-          minColor: '#BFCFAD',
-          maxColor: '#31784B',
-          max: 1000,
-        },
+  //       colorAxis: {
+  //         tickPixelInterval: 100,
+  //         minColor: '#BFCFAD',
+  //         maxColor: '#31784B',
+  //         max: 1000,
+  //       },
 
-        // tooltip: {
-        //   formatter: () => {
-        //     if (chart.mapView.point.name && chart.mapView.point.value) {
-        //       return `${chart.mapView.point.name}: ${chart.mapView.point.value}`;
-        //     } else {
-        //       return false; // now you don't
-        //     }
-        //   },
-        // },
+  //       // tooltip: {
+  //       //   formatter: () => {
+  //       //     if (chart.mapView.point.name && chart.mapView.point.value) {
+  //       //       return `${chart.mapView.point.name}: ${chart.mapView.point.value}`;
+  //       //     } else {
+  //       //       return false; // now you don't
+  //       //     }
+  //       //   },
+  //       // },
 
-        plotOptions: {
-          series: {
-            animation: {
-              duration: 750,
-            },
-            clip: false,
-            marker: {
-              fillColor: '#FFFFFF',
-              lineWidth: 2,
-              lineColor: 'green',
-            },
-          },
-          mapline: {
-            color: 'green',
-          },
-        },
-        series: [
-          // Globe Latitudes and Longitudes Lines
-          {
-            id: 'graticule',
-            type: 'mapline',
-            data: this.getGraticule(),
-            showInLegend: false,
+  //       plotOptions: {
+  //         series: {
+  //           animation: {
+  //             duration: 750,
+  //           },
+  //           clip: false,
+  //           marker: {
+  //             fillColor: '#FFFFFF',
+  //             lineWidth: 2,
+  //             lineColor: 'green',
+  //           },
+  //         },
+  //         mapline: {
+  //           color: 'green',
+  //         },
+  //       },
+  //       series: [
+  //         // Globe Latitudes and Longitudes Lines
+  //         {
+  //           id: 'graticule',
+  //           type: 'mapline',
+  //           data: this.getGraticule(),
+  //           showInLegend: false,
 
-            //Map Color where the globe is joint
-            nullColor: 'rgba(0, 0, 0, 0.05)',
-            accessibility: {
-              enabled: false,
-            },
-          },
-          // The Official Data Series on the Globe
-          {
-            data: this.mapData,
-            type: 'map',
-            joinBy: 'name',
-            name: 'Total Login per million km²',
-            enableMouseTracking: true,
-            states: {
-              hover: {
-                color: '#a4edba',
-                borderColor: '#333333',
-              },
-            },
-            dataLabels: {
-              enabled: false,
-              format: '{point.name}',
-            },
+  //           //Map Color where the globe is joint
+  //           nullColor: 'rgba(0, 0, 0, 0.05)',
+  //           accessibility: {
+  //             enabled: false,
+  //           },
+  //         },
+  //         // The Official Data Series on the Globe
+  //         {
+  //           data: this.mapData,
+  //           type: 'map',
+  //           joinBy: 'name',
+  //           name: 'Total Login per million km²',
+  //           enableMouseTracking: true,
+  //           states: {
+  //             hover: {
+  //               color: '#a4edba',
+  //               borderColor: '#333333',
+  //             },
+  //           },
+  //           dataLabels: {
+  //             enabled: false,
+  //             format: '{point.name}',
+  //           },
 
-            accessibility: {
-              enabled: true,
-            },
-          },
-          // PinPoints 📍 on the Globe
-          {
-            data: [
-              {
-                name: 'London',
-                id: 'London',
-                lat: 51.507222,
-                lon: -0.1275,
-              },
-              {
-                name: 'Birmingham',
-                id: 'Birmingham',
-                lat: 52.483056,
-                lon: -1.893611,
-              },
-              {
-                name: 'Leeds',
-                id: 'Leeds',
-                lat: 53.799722,
-                lon: -1.549167,
-              },
-              {
-                name: 'Glasgow',
-                id: 'Glasgow',
-                lat: 55.858,
-                lon: -4.259,
-              },
-              {
-                name: 'Sheffield',
-                id: 'Sheffield',
-                lat: 53.383611,
-                lon: -1.466944,
-              },
-              {
-                name: 'Liverpool',
-                id: 'Liverpool',
-                lat: 53.4,
-                lon: -3,
-              },
-              {
-                name: 'Bristol',
-                id: 'Bristol',
-                lat: 51.45,
-                lon: -2.583333,
-              },
-              {
-                name: 'Belfast',
-                id: 'Belfast',
-                lat: 54.597,
-                lon: -5.93,
-              },
-              {
-                name: 'Lerwick',
-                id: 'Lerwick',
-                lat: 60.155,
-                lon: -1.145,
-                dataLabels: {
-                  align: 'left',
-                  x: 5,
-                  verticalAlign: 'middle',
-                },
-              },
-            ],
-            type: 'mappoint',
-          },
-          ,
-          {
-            name: 'London flight routes',
-            type: 'mapline',
-            lineWidth: 2,
-            color: '#333333',
-            borderColor: '#333333',
-            dashStyle: 'line',
-            // color: this.Highcharts.setOptions({color:'green'} as Highcharts.Options),
-            data: [
-              {
-                id: 'London - Glasgow',
-                path: pointsToPath(chart.get('London'), chart.get('Glasgow')),
-              },
-              {
-                id: 'London - Belfast',
-                path: pointsToPath(
-                  chart.get('London'),
-                  chart.get('Belfast'),
-                  true
-                ),
-              },
-              {
-                id: 'London - Leeds',
-                path: pointsToPath(chart.get('London'), chart.get('Leeds')),
-              },
-              {
-                id: 'London - Liverpool',
-                path: pointsToPath(
-                  chart.get('London'),
-                  chart.get('Liverpool'),
-                  true
-                ),
-              },
-              {
-                id: 'London - Sheffield',
-                path: pointsToPath(chart.get('London'), chart.get('Sheffield')),
-              },
-              {
-                id: 'London - Birmingham',
-                path: pointsToPath(
-                  chart.get('London'),
-                  chart.get('Birmingham'),
-                  true
-                ),
-              },
-              {
-                id: 'London - Bristol',
-                path: pointsToPath(
-                  chart.get('London'),
-                  chart.get('Bristol'),
-                  true
-                ),
-              },
-            ],
-          } as any,
-        ],
-      };
-    };
-    console.log('Map Updated');
-    console.log(this.map);
-  }
+  //           accessibility: {
+  //             enabled: true,
+  //           },
+  //         },
+  //         // PinPoints 📍 on the Globe
+  //         {
+  //           data: [
+  //             {
+  //               name: 'London',
+  //               id: 'London',
+  //               lat: 51.507222,
+  //               lon: -0.1275,
+  //             },
+  //             {
+  //               name: 'Birmingham',
+  //               id: 'Birmingham',
+  //               lat: 52.483056,
+  //               lon: -1.893611,
+  //             },
+  //             {
+  //               name: 'Leeds',
+  //               id: 'Leeds',
+  //               lat: 53.799722,
+  //               lon: -1.549167,
+  //             },
+  //             {
+  //               name: 'Glasgow',
+  //               id: 'Glasgow',
+  //               lat: 55.858,
+  //               lon: -4.259,
+  //             },
+  //             {
+  //               name: 'Sheffield',
+  //               id: 'Sheffield',
+  //               lat: 53.383611,
+  //               lon: -1.466944,
+  //             },
+  //             {
+  //               name: 'Liverpool',
+  //               id: 'Liverpool',
+  //               lat: 53.4,
+  //               lon: -3,
+  //             },
+  //             {
+  //               name: 'Bristol',
+  //               id: 'Bristol',
+  //               lat: 51.45,
+  //               lon: -2.583333,
+  //             },
+  //             {
+  //               name: 'Belfast',
+  //               id: 'Belfast',
+  //               lat: 54.597,
+  //               lon: -5.93,
+  //             },
+  //             {
+  //               name: 'Lerwick',
+  //               id: 'Lerwick',
+  //               lat: 60.155,
+  //               lon: -1.145,
+  //               dataLabels: {
+  //                 align: 'left',
+  //                 x: 5,
+  //                 verticalAlign: 'middle',
+  //               },
+  //             },
+  //           ],
+  //           type: 'mappoint',
+  //         },
+  //         {
+  //           type: 'mapline',
+  //           name: 'Flight route, Amsterdam - Los Angeles',
+  //           animation: false,
+  //           id: 'flight-route',
+  //           data: [
+  //             {
+  //               geometry: {
+  //                 type: 'LineString',
+  //                 coordinates: [
+  //                   [4.9, 53.38], // Amsterdam
+  //                   [-118.24, 34.05], // Los Angeles
+  //                 ],
+  //               },
+  //               color: '#313f77',
+  //             },
+  //           ],
+  //           lineWidth: 2,
+  //           accessibility: {
+  //             exposeAsGroupOnly: true,
+  //           },
+  //         },
+  //         {
+  //           name: 'London flight routes',
+  //           type: 'mapline',
+  //           lineWidth: 2,
+  //           color: '#333333',
+  //           borderColor: '#333333',
+  //           dashStyle: 'line',
+  //           // color: this.Highcharts.setOptions({color:'green'} as Highcharts.Options),
+  //           data: [
+  //             {
+  //               id: 'London - Glasgow',
+  //               path: pointsToPath(chart.get('London'), chart.get('Glasgow')),
+  //             },
+  //             {
+  //               id: 'London - Belfast',
+  //               path: pointsToPath(
+  //                 chart.get('London'),
+  //                 chart.get('Belfast'),
+  //                 true
+  //               ),
+  //             },
+  //             {
+  //               id: 'London - Leeds',
+  //               path: pointsToPath(chart.get('London'), chart.get('Leeds')),
+  //             },
+  //             {
+  //               id: 'London - Liverpool',
+  //               path: pointsToPath(
+  //                 chart.get('London'),
+  //                 chart.get('Liverpool'),
+  //                 true
+  //               ),
+  //             },
+  //             {
+  //               id: 'London - Sheffield',
+  //               path: pointsToPath(chart.get('London'), chart.get('Sheffield')),
+  //             },
+  //             {
+  //               id: 'London - Birmingham',
+  //               path: pointsToPath(
+  //                 chart.get('London'),
+  //                 chart.get('Birmingham'),
+  //                 true
+  //               ),
+  //             },
+  //             {
+  //               id: 'London - Bristol',
+  //               path: pointsToPath(
+  //                 chart.get('London'),
+  //                 chart.get('Bristol'),
+  //                 true
+  //               ),
+  //             },
+  //           ],
+  //         } as any,
+  //       ],
+  //     };
+  //   };
+  //   console.log('Map Updated');
+  //   console.log(this.map);
+  // }
   Globe: any;
 
   chartCallback: Highcharts.ChartCallbackFunction | any;
@@ -522,6 +543,29 @@ export class MapComponent {
           },
         ],
         type: 'mappoint',
+      },
+      // Routes
+      {
+        type: 'mapline',
+        name: 'Flight route, Amsterdam - Los Angeles',
+        animation: false,
+        id: 'flight-route',
+        data: [
+          {
+            geometry: {
+              type: 'LineString',
+              coordinates: [
+                [4.9, 53.38], // Amsterdam
+                [-118.24, 34.05], // Los Angeles
+              ],
+            },
+            color: '#313f77',
+          },
+        ],
+        lineWidth: 2,
+        accessibility: {
+          exposeAsGroupOnly: true,
+        },
       },
     ],
   };
